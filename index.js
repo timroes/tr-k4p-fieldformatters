d168cb4319a58a7378dd3f6ab14bc6d74186dd5b
Tim Roes <github@timroes.de>
2016-02-04 18:11:29 +0100

    chore: add link to github issue for fieldFormats

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = function (kibana) {
     require: ['kibana', 'elasticsearch'],
     uiExports: {
       // fieldFormat is not working as of Kibana 4.3.0, but it's fixed in the master branch
+      // see the issue on GitHub: https://github.com/elastic/kibana/issues/6040
       // As a workaround, just add the field formats to visTypes array.
       fieldFormats: ['plugins/tr-k4p-fieldformatters/trend']
     }
